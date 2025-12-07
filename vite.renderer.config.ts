7808994b2c7a5987992fe333df0e8797747e989b
Markus Rother <mail@markusrother.de>
2025-12-07 09:48:34 +0100

    fixed npm run dev

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   root: path.resolve(__dirname, 'src/renderer'),
   base: './',
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
   build: {
     outDir: path.resolve(__dirname, 'dist/renderer'),
     emptyOutDir: true,
